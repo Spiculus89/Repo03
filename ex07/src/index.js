9c03470402e1d57678ecad28b6c837580c7b0548
Amar Alibegovic <amaralibegovic89@gmail.com>
2021-09-16 16:55:45 +0200

    fixed some lines

--- ex07/src/index.js
+++ ex07/src/index.js
@@ -1,16 +1,16 @@
 function main() {
     // Only change code below this line
     // Variable declarations
-        var sturdlyCapVar;
+        var studlyCapVar;
         var properCamelCase;
         var titleCaseOver;
     // Variable assignments
-        sturdlyCapVar = 10;
+        studlyCapVar = 10;
         properCamelCase = "A String";
         titleCaseOver = 9000;
     // Only change code above this line
     return {
-        sturdlyCapVar,
+        studlyCapVar,
         properCamelCase,
         titleCaseOver
     }
